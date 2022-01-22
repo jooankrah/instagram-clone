62754df560a5466ecaa7ec284c63b8750a8faa6b
jonathan_nsiah <jooankrah@gmail.com>
2022-01-22 16:13:35 +0000

    refactor :  refactor stories card

--- components/stories.js
+++ components/stories.js
@@ -3,11 +3,12 @@ import StoryItem from './storyItem'
 
 export default function Stories() {
     return (
-        <div className='flex flex-row items-center space-x-3 p-3 overflow-scroll scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 scrollbar-hide md:scrollbar-default'>
+        <div className='flex flex-row items-start space-x-3 p-5 overflow-scroll scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 scrollbar-hide'>
             <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my name' />
             <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my namesdggadgehterhr' />
             <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my name' />
             <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my name' />
+            {/* <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my name' />
             <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my name' />
             <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my name' />
             <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my name' />
@@ -19,8 +20,7 @@ export default function Stories() {
             <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my name' />
             <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my name' />
             <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my name' />
-            <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my name' />
-            <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my name' />
+            <StoryItem image={'https://pbs.twimg.com/profile_images/1296568929209458689/xLXGAOC1_400x400.jpg'} name='my name' /> */}
         </div>
     )
 }
